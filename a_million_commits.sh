fe0bc34804c1b289c9cddba5c315d5fa965461ae
Joshua Anop <rolexy.inc@gmail.com>
2020-03-31 23:39:37 +0100

    Change

---
+++ a_million_commits.sh
@@ -0,0 +1,33 @@
+# a million commits
+for Y in {2020..2020}
+do
+  mkdir $Y
+  cd $Y
+  for M in {03..03}
+  do
+    mkdir $M
+    cd $M
+    for D in {20..28}
+    do
+      mkdir $D
+      cd $D
+      for i in {01..6}
+      do
+        echo "$i on $M/$D/$Y" > commit.md
+        
+        export GIT_COMMITTER_DATE="$Y-$M-$D 12:$i:00"
+        export GIT_AUTHOR_DATE="$Y-$M-$D 12:$i:00"
+        git add commit.md -f
+        git commit --date="$Y-$M-$D 12:0$i:00" -m "$i on $M $D $Y"
+      done
+      cd ../
+    done
+    cd ../
+  done
+  cd ../
+done
+git push origin master
+git rm -rf 20**
+git rm -rf 19**
+git commit -am "cleanup"
+git push origin master
